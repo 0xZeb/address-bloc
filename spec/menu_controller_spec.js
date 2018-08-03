5b65bc34a785236bbd4f36675c56f0a36359d085
Zeb Smithson <zebsmithson@yahoo.com>
2018-08-02 22:01:22 -0700

    checkpoit testing complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -4,7 +4,13 @@ describe("MenuController", () => {
 
   beforeEach(() => {
     this.menu = new MenuController();
-    
+
+  });
+
+  describe("#remindMe()", () => {
+    it("should return a string", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+    });
   });
 
   describe("#getContactCount()", () => {
