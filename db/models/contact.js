408cad5b2b1f2efcd8d44824a6b93f8ed383cedf
Zeb Smithson <zebsmithson@yahoo.com>
2018-08-09 21:14:41 -0700

    assignment complete

--- db/models/contact.js
+++ db/models/contact.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Contact = sequelize.define('Contact', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING
   }, {});
   Contact.associate = function(models) {
     // associations can be defined here
