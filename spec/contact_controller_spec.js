8b1e847f4d659178913c7e1a82bcbc4f228727d2
Zeb Smithson <zebsmithson@yahoo.com>
2018-08-10 11:44:13 -0700

    updated test spec

---
+++ spec/contact_controller_spec.js
@@ -0,0 +1,65 @@
+const ContactController = require("../controllers/ContactController");
+const sequelize = require("../db/models/index").sequelize;
+
+
+describe("ContactController", () => {
+
+  beforeEach((done) => {
+    this.book = new ContactController();
+
+    sequelize.sync({force: true}).then((res) => {
+      done();
+    })
+    .catch((err) => {
+      done();
+    });
+  });
+
+
+  describe("#addContact()", () => {
+
+    it("should add a single contact into the book", (done) => {
+      this.book.addContact("Alice", "001-101-1010", "alice@alice.com")
+      .then((contact) => {
+
+        expect(contact.name).toBe("Alice");
+        expect(contact.phone).toBe("001-101-1010");
+        expect(contact.email).toBe("alice@alice.com");
+        done();
+
+      })
+      .catch((err) => {
+        done();
+      });
+    });
+
+  });
+
+  describe("#getContacts()", () => {
+    it("should return an empty array when no contacts are available", (done) => {
+        this.book.getContacts()
+        .then((contacts) => {
+            expect(contacts.length).toBe(0);
+            done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });  
+  });
+
+  it("should return an array of contacts when contacts are available", (done) => {
+    this.book.addContact("Alice", "001-101-1010", "alice@alice.com")
+    .then(() => {
+      this.book.getContacts()
+      .then((contacts) => {
+        expect(contacts.length).toBe(1);
+        done();
+      });
+    });
+  }); //end IT
+
+
+
+}); //end describe OUTER
