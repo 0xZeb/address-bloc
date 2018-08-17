d4f39188bf5215ab68089ad3061dca3d70795282
Zeb Smithson <zebsmithson@yahoo.com>
2018-08-17 07:08:55 -0700

    checkpoint work complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,12 +10,14 @@ module.exports = class MenuController {
          message: "Please choose from an option below: ",
          choices: [
            "Add new contact",
+           "View all contacts",
            "Get Contacts",
-           "Remind me",
+           "Search for a contact",
            "Exit"
          ]
        }
      ];
+
      this.book = new ContactController();
   }
 
@@ -26,11 +28,11 @@ module.exports = class MenuController {
        case "Add new contact":
          this.addContact();
          break;
-       case "Get contacts":
-         this.getContactCount();
+       case "View all contacts":
+         this.getContacts();
          break;
-       case "Remind me":
-         this.remindMe();
+       case "Search for a contact":
+         this.search();
          break;
        case "Exit":
          this.exit();
@@ -66,10 +68,89 @@ module.exports = class MenuController {
     process.exit();
   }
 
-  remindMe(){
-    return "Learning is a life-long pursuit";
-    this.main();
+  getContacts(){
+    this.clear();
+    this.book.getContacts().then((contacts) => {
+        for( let contact of contacts) {
+          console.log(`
+            name: ${contact.name}
+            phone number: ${contact.phone}
+            email: ${contact.email}
+            ----------------`
+          );
+        }
+        this.main();
+    }).catch((err) => {
+      console.log(err);
+      this.main();
+    });
   }
 
+  search(){
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+      this.book.search(target.name)
+      .then((contact) => {
+        if(contact === null){
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  showContact(contact){
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+  }
+
+
+  delete(contact){
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then((answer) => {
+      if(answer.confirmation){
+        this.book.delete(contact.id);
+        console.log("contact deleted!");
+        this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  _printContact(contact){
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      -------------`
+    );
+  }
 
 }
